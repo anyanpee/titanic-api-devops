38c591b9923f9ab46bbecaea71b6f94dd4be382b
peter anyankpele <anyankpelepeter7@gmail.com>
2026-01-22 10:57:05 -0800

    style: final PEP8 blank lines fixed

--- src/models/person.py
+++ src/models/person.py
@@ -2,18 +2,24 @@ from __future__ import annotations
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from . import db
+
+
 class Person(db.Model):
     """
     Database model for storing the data of people on the Titanic.
     """
+
     __tablename__ = "people"
+
     uuid = db.Column(
         UUID(as_uuid=True),
         primary_key=True,
         default=uuid.uuid4,
     )
+
     survived = db.Column(db.Integer)
     passenger_class = db.Column(db.Integer)
+
     @classmethod
     def get_all(cls):
         return cls.query.all()
