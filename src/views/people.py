907ef2e10fdaaba448a46b2ca21ac51958f51687
peter anyankpele <anyankpelepeter7@gmail.com>
2026-01-22 10:54:16 -0800

    style: final PEP8 clean People view

--- src/views/people.py
+++ src/views/people.py
@@ -1,11 +1,17 @@
 from flask import Blueprint, Response, json
+
 from ..models.person import Person, PersonSchema
+
+
 people_api = Blueprint("people", __name__)
 person_schema = PersonSchema()
+
+
 @people_api.route("/people", methods=["GET"])
 def get_all() -> Response:
     """
     Endpoint returning all people from the database.
+
     Returns:
         Response containing all people.
     """
